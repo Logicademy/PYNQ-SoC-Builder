a045b49817d5f2c59a02ed8a9e0422c6984a3685
lukecanny <lcanny8@gmail.com>
2023-11-06 22:49:58 +0000

    minor text changes

--- pynqautomate.py
+++ pynqautomate.py
@@ -1,16 +1,16 @@
 import argparse
 import pynq_manager as pm
 
-argparser = argparse.ArgumentParser(description="A simple CLI program with subcommands")
+argparser = argparse.ArgumentParser(description="PynqAutomate is a CLI tool used to automate the build process for HDLGen projects")
 
-subparsers = argparser.add_subparsers(title="commands", dest="command", description="is this description ever seen?")
+subparsers = argparser.add_subparsers(title="commands", dest="command")
 
 # Sub Command 1: generate_tcl
-parser_command1 = subparsers.add_parser("generate_tcl", help="Generate the Tcl script used by Vivado to create BD, synthesis, implement and generate bitstream!")
+parser_command1 = subparsers.add_parser("generate_tcl", help="Generate the Tcl script used by Vivado to create BD, synthesis, implement and generate bitstream")
 parser_command1.add_argument("--hdlgen", help="Path to HDLGen file")
 
 # Sub Command 2: run_vivado
-parser_command2 = subparsers.add_parser("run_vivado", help="Runs the Tcl script created by the generate_tcl command!")
+parser_command2 = subparsers.add_parser("run_vivado", help="Runs the Tcl script created by the generate_tcl command")
 parser_command2.add_argument("--hdlgen", help="Path to HDLGen file")
 
 # Parse the command-line arguments
