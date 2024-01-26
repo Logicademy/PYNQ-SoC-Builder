5c1a29764b084ea321c9b9db44778176a72a61c6
lukecanny <lcanny8@gmail.com>
2024-01-26 10:16:33 +0000

    prev commit shouldn't have been empty

--- main_gui.py
+++ main_gui.py
@@ -8,10 +8,13 @@ app = ctk.CTk()
 app.title("PYNQ SoC Builder")
 app.geometry("500x240")
 
+
+## Row 0
 # Title Label
 title_font = ("Segoe UI", 20, "bold") # Title font
-title_label = ctk.CTkLabel(app, text="PYNQ SoC Builder", font=title_font, pady=5)
+title_label = ctk.CTkLabel(app, text="PYNQ SoC Builder", font=title_font)
 
+## Row 1
 # File path entry and browse button
 def browse_files():
     file_path = ctk.filedialog.askopenfilename(filetypes=[("HDLGen Files", "*.hdlgen")])
@@ -21,15 +24,25 @@ def browse_files():
 entry_path = ctk.CTkEntry(app, width=360)
 browse_button = ctk.CTkButton(app, text="Browse", command=browse_files, width=100)
 
-# title_label.pack(pady=10)
-# entry_path.pack(pady=5, side=ctk.LEFT)
-# browse_button.pack(pady=5, padx=5, side=ctk.LEFT)
+## Row 2
+mode_font = ("Segoe UI", 16)
+mode_label = ctk.CTkLabel(app, text="Mode", font=mode_font, pady=5, width=20)
 
-title_label.grid(row=0, column=0, columnspan=2)
+mode_menu_options = ["Run All", "Generate Tcl", "Run Vivado", "Copy Bitstream", "Create JNB /w Testplan", "Create JNB w/o Testplan"]
+mode_menu_var = ctk.StringVar(app)
+mode_menu_var.set(mode_menu_options[0])
+
+def on_mode_dropdown(choice):
+    print("Selected option: ", choice)
+
+mode_dropdown = ctk.CTkOptionMenu(app, variable=mode_menu_var, values=mode_menu_options, command=on_mode_dropdown, width=150)
+
+
+title_label.grid(row=0, column=0, columnspan=2, pady=5)
 entry_path.grid(row=1, column=0, padx=10, pady=5)
 browse_button.grid(row=1, column=1, padx=10, pady=5)
-
-
+mode_label.grid(row=2, column=0, pady=5, padx=10)
+mode_dropdown.grid(row=2, column=1, pady=5, padx=10)
 
 # def button_function():
 #     print("Button Pressed")
