6c5f72e3a498fef1b740b729565e489f00591c71
lukecanny <lcanny8@gmail.com>
2024-02-17 13:48:47 +0000

    redesigned config menu - removed automatic option as it is too complicated. Simple implementation still there

--- main_gui.py
+++ main_gui.py
@@ -32,7 +32,12 @@ class Application:
         # Shared flags
         self.build_running = False              # If build process is running, this flag will be True
         self.subprocess_exit_signal = threading.Event()    # If force closed, threading.Event() used to signal close to running threads.
-        self.io_config_set = False
+        self.io_configuration = {
+            "led0":"None",
+            "led1":"None",
+            "led2":"None",
+            "led3":"None"
+        }
 
         # Shared Tcl Generator Flags
         self.skip_board_config = False
@@ -593,8 +598,9 @@ class IO_Config_Window(ctk.CTkToplevel):
         self.app = app
         self.title("Configure Board IO")
         self.grab_set() # This makes the pop-up the primary window and doesn't allow interaction with main menu
-        self.geometry("400x230")
-        icon_font = ("Segoe Fluent Icons Regular", 80)
+        self.geometry("300x230")
+        # {'family': 'Segoe UI', 'size': 9, 'weight': 'normal', 'slant': 'roman', 'underline': 0, 'overstrike': 0}
+        header_font = ("Segoe UI", 16, "bold") # Title font
         self.resizable(False, False) # Dont let the window be resizable
         
         # Need to learn the I/O thats available - What top level signals can we use.
@@ -612,52 +618,57 @@ class IO_Config_Window(ctk.CTkToplevel):
 
 
         for port in port_map:
+
             gpio_name = port[0]   # GPIO Name
             gpio_mode = port[1]   # GPIO Mode (in/out)
             gpio_type = port[2]   # GPIO Type (single bit/bus/array)
             gpio_width = 0
+
             if (gpio_type == "single bit"):
-                gpio_width = 1
+                # gpio_width = 1
+                port_map_signals_names.append(gpio_name)
+            
             elif (gpio_type[:3] == "bus"):
                 # <type>bus(31 downto 0)</type>     ## Example Type Value
                 substring = gpio_type[4:]           # substring = '31 downto 0)'
                 words = substring.split()           # words = ['31', 'downto', '0)']
                 gpio_width = int(words[0]) + 1           # words[0] = 31
+                for i in range(gpio_width):
+                    port_map_signals_names.append(f"{gpio_name}[{i}]")
 
-            # port_map_signals_names.append(sub_array[0])
-            for i in range(gpio_width):
-                port_map_signals_names.append(f"{gpio_name}[{i}]")
 
+        # # # # Automatic Mode is Disabled for the Moment - Realistically it just adds too much complexicity for only a few LEDs.
+        # # # # Ultimately not very scalable either.
 
         # Left frame
-        self.left_frame = ctk.CTkFrame(self, width=100, height=100)  # Left frame will contain checkbox for yes/no auto scanning + explaination
+        # self.left_frame = ctk.CTkFrame(self, width=100, height=100)  # Left frame will contain checkbox for yes/no auto scanning + explaination
 
-        self.l_top_label = ctk.CTkLabel(self.left_frame, width=200, height=30, text="Auto IO")
-        self.l_top_label.grid(row=0, column=0, pady=5)
+        # self.l_top_label = ctk.CTkLabel(self.left_frame, width=200, height=30, text="Auto IO")
+        # self.l_top_label.grid(row=0, column=0, pady=5)
 
         # Left frame set up
-        self.auto_io_checkbox = ctk.CTkCheckBox(self.left_frame, text="Auto Detect IO", width=100)
-        self.auto_io_checkbox.grid(row=1, column=0)
+        # self.auto_io_checkbox = ctk.CTkCheckBox(self.left_frame, text="Auto Detect IO", width=100)
+        # self.auto_io_checkbox.grid(row=1, column=0)
         # text="In automatic mode, signals tagged with an IO related suffix (eg '_led') will be assigned to I/O. The suffix mapping is as follows:\n - _led: Any available port\n- _ledx: LEDx only (x=0,1,2,3)"
-        self.auto_io_label = ctk.CTkLabel(self.left_frame, wraplength=100, width=100, anchor="nw", text="\nExplaination of how Auto mode works", height=130, corner_radius=0)
-        self.auto_io_label.grid(row=2, column=0)
+        # self.auto_io_label = ctk.CTkLabel(self.left_frame, wraplength=100, width=100, anchor="nw", text="\nExplaination of how Auto mode works", height=130, corner_radius=0)
+        # self.auto_io_label.grid(row=2, column=0)
         
         # Right frame
         self.right_frame = ctk.CTkFrame(self, width=200, height=100) # Right frame will contain manual config of signals
 
-        self.r_top_label = ctk.CTkLabel(self.right_frame, width=200, height=30, text="Manual IO", corner_radius=0)
+        self.r_top_label = ctk.CTkLabel(self.right_frame, width=200, height=30, text="Configure Board I/O Connections", corner_radius=0, font=header_font)
         self.r_top_label.grid(row=0, column=0, columnspan=2, pady=5)
 
 
-        self.led_0_label = ctk.CTkLabel(self.right_frame, text="LED0", width=40)
-        self.led_1_label = ctk.CTkLabel(self.right_frame, text="LED1", width=40)
-        self.led_2_label = ctk.CTkLabel(self.right_frame, text="LED2", width=40)
-        self.led_3_label = ctk.CTkLabel(self.right_frame, text="LED3", width=40)
+        self.led_0_label = ctk.CTkLabel(self.right_frame, text="LED0", width=140)
+        self.led_1_label = ctk.CTkLabel(self.right_frame, text="LED1", width=140)
+        self.led_2_label = ctk.CTkLabel(self.right_frame, text="LED2", width=140)
+        self.led_3_label = ctk.CTkLabel(self.right_frame, text="LED3", width=140)
 
-        self.led_0_label.grid(row=1, column=0, pady=5)
-        self.led_1_label.grid(row=2, column=0, pady=5)
-        self.led_2_label.grid(row=3, column=0, pady=5)
-        self.led_3_label.grid(row=4, column=0, pady=5)
+        self.led_0_label.grid(row=1, column=0, pady=5, padx=5)
+        self.led_1_label.grid(row=2, column=0, pady=5, padx=5)
+        self.led_2_label.grid(row=3, column=0, pady=5, padx=5)
+        self.led_3_label.grid(row=4, column=0, pady=5, sticky="nsew", padx=5)
 
         # mode_dropdown = ctk.CTkOptionMenu(self.row_2_frame, variable=mode_menu_var, values=self.mode_menu_options, command=on_mode_dropdown, width=150)
         def _on_io_dropdown(args):
@@ -665,38 +676,41 @@ class IO_Config_Window(ctk.CTkToplevel):
             pass
 
         dropdown_options = port_map_signals_names
-        dropdown_options.insert(0, "No I/O")
+        dropdown_options.insert(0, "None")
 
-        self.led_0_var = ctk.StringVar(value="No I/O")
+        self.led_0_var = ctk.StringVar(value=self.app.io_configuration["led0"]) # self.io_configuration["led0"]
         self.led_0_dropdown = ctk.CTkOptionMenu(self.right_frame, variable=self.led_0_var, values=dropdown_options, command=_on_io_dropdown)
 
-        self.led_1_var = ctk.StringVar(value="No I/O")
+        self.led_1_var = ctk.StringVar(value=self.app.io_configuration["led1"])
         self.led_1_dropdown = ctk.CTkOptionMenu(self.right_frame, variable=self.led_1_var, values=dropdown_options, command=_on_io_dropdown)
 
-        self.led_2_var = ctk.StringVar(value="No I/O")
+        self.led_2_var = ctk.StringVar(value=self.app.io_configuration["led2"])
         self.led_2_dropdown = ctk.CTkOptionMenu(self.right_frame, variable=self.led_2_var, values=dropdown_options, command=_on_io_dropdown)
 
-        self.led_3_var = ctk.StringVar(value="No I/O")
+        self.led_3_var = ctk.StringVar(value=self.app.io_configuration["led3"])
         self.led_3_dropdown = ctk.CTkOptionMenu(self.right_frame, variable=self.led_3_var, values=dropdown_options, command=_on_io_dropdown)
 
-        self.led_0_dropdown.grid(row=1, column=1, pady=5)
-        self.led_1_dropdown.grid(row=2, column=1, pady=5)
-        self.led_2_dropdown.grid(row=3, column=1, pady=5)
-        self.led_3_dropdown.grid(row=4, column=1, pady=5)
+        self.led_0_dropdown.grid(row=1, column=1, pady=5, padx=5)
+        self.led_1_dropdown.grid(row=2, column=1, pady=5, padx=5)
+        self.led_2_dropdown.grid(row=3, column=1, pady=5, padx=5)
+        self.led_3_dropdown.grid(row=4, column=1, pady=5, padx=5)
 
 
         # Right frame set up
     	# Button handlers
         def _on_save_button():
-            # Set the IO config in shared space.
+            # Set the IO config in shared space
+            self.app.io_configuration["led0"] = self.led_0_var.get()
+            self.app.io_configuration["led1"] = self.led_1_var.get()
+            self.app.io_configuration["led2"] = self.led_2_var.get()
+            self.app.io_configuration["led3"] = self.led_3_var.get()
             # Close Window
-            # Set a flag to say that the config was defined.
             self.destroy()
 
         def _on_cancel_button():
             # We don't care about setting any values
             # If cancel pressed, simply close the menu.
-            self.destroy()  
+            self.destroy()
 
         self.button_frame = ctk.CTkFrame(self, width=300, height=50) # Button frame will remain the same as Dialog window
 
@@ -707,9 +721,9 @@ class IO_Config_Window(ctk.CTkToplevel):
         self.cancel_button = ctk.CTkButton(self.button_frame, text="Cancel", command=_on_cancel_button)
         self.cancel_button.grid(row=0, column=1, pady=5, padx=5)
 
-        self.left_frame.grid(column=0, row=0)
-        self.right_frame.grid(column=1, row=0)
-        self.button_frame.grid(column=0, row=1, columnspan=2)
+        # self.left_frame.grid(column=0, row=0)
+        self.right_frame.grid(column=0, row=0)
+        self.button_frame.grid(column=0, row=1) # , columnspan=2
 
     def get_io_ports(self):
         hdlgen = xml.dom.minidom.parse(self.app.hdlgen_path)
