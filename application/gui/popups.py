f2be1e514583df319b27bc1e239eae044fd9da07
lukecanny <lcanny8@gmail.com>
2024-02-29 15:09:08 +0000

    added remote data, new pop-up menu and screenshot
    backend code for the menu isn't added yet

--- application/gui/popups.py
+++ application/gui/popups.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+import csv
 
 class Alert_Window(ctk.CTkToplevel):
     def __init__(self, app):
@@ -68,3 +69,78 @@ class Dialog_Window(ctk.CTkToplevel):
         self.left_frame.grid(column=0, row=0)
         self.right_frame.grid(column=1, row=0)
         self.button_frame.grid(column=0, row=1, columnspan=2)
+
+class Remote_Window(ctk.CTkToplevel):
+    def __init__(self, app):
+        ctk.CTkToplevel.__init__(self, app.root)
+        self.app = app
+        self.title("Open PYNQ Remote Webpages")
+        self.grab_set() # This makes the pop-up the primary window and doesn't allow interaction with main menu
+        self.geometry("300x100")
+        # self.resizable(False, False) # Dont let the window be resizable
+        
+        # We are going to read from a file which contains the data
+        # All the info related to remote servers is now stored in /application/remote.csv
+        # This file could be upgraded to a XML file in the future but CSV is fine for now
+        csv_file_path = "application/remote.csv"
+        csv_data = []
+        error_message = None
+        try:
+            with open(csv_file_path, 'r') as file:
+                csv_reader = csv.reader(file)
+                
+                for row in csv_reader:
+                    # Ignore lines starting with #
+                    if not row or row[0].startswith('#'):
+                        continue
+                    csv_data.append(row)
+        except FileNotFoundError:
+            error_message = "\nFile 'remote.csv' file is missing. Remote servers cannot be listed."
+
+        if csv_data == []:
+            # If the CSV data is empty, I want to show error menu
+            icon_font = ("Segoe Fluent Icons Regular", 80)
+            
+            self.left_frame = ctk.CTkFrame(self, width=100, height=100)
+            self.right_frame = ctk.CTkFrame(self, width=200, height=100)
+
+            self.label = ctk.CTkLabel(self.left_frame, text="\uedae", font=icon_font, width=100, height=100)
+            self.label.pack()
+            
+            if error_message:
+                self.error_text = error_message
+            else:
+                self.error_text = "\nNo Servers Listed in Config"
+            
+            self.msglabel = ctk.CTkLabel(self.right_frame, text=self.error_text, width=200, height=100, wraplength=180, anchor="n")
+            self.msglabel.pack()
+            
+            self.left_frame.grid(column=0, row=0)
+            self.right_frame.grid(column=1, row=0)
+        else:
+            header_font = ("Segoe UI", 16, "bold") # Title font
+            self.popup_frame = ctk.CTkFrame(self, width=300, height=100) # Main frame to store everything
+
+            self.top_label = ctk.CTkLabel(self.popup_frame, width=200, height=30, text="Open Remote Lab", corner_radius=0, font=header_font)
+            self.top_label.grid(row=0, column=0, pady=5)
+            # Run a loop for each line in the CSV
+            current_row = 1
+            for link in csv_data:
+                print(link)
+                # link = [name, url]
+                button = ctk.CTkButton(self.popup_frame, text=link[0], width=140)
+                button.grid(row=current_row, column=0, pady=5, padx=5)
+                # Link is gonna be handled by the button handler I suppose.
+                current_row = current_row + 1
+
+            # Here we need to add close button
+            return_button = ctk.CTkButton(self.popup_frame, text="Return", width=140)
+            return_button.grid(row=current_row, column=0, padx=5, pady=5)
+        
+            window_width = 200
+            window_height = (len(csv_data) + 1) * 40 + 30
+            window_size = f"{str(window_width)}x{str(window_height)}"
+            self.geometry(window_size)
+
+        self.popup_frame.pack()
+        
