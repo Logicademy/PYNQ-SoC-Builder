67ec6eac0cf428bd28bdc5e3369ee521675b9374
lukecanny <lcanny8@gmail.com>
2024-03-01 18:17:32 +0000

    created xml_manager stub for creating and managing pynqbuild xml file

---
+++ application/xml_manager.py
@@ -0,0 +1,15 @@
+import xml.dom.minidom
+
+class Xml_Manager:
+    def __init__(self, hdlgen_path):
+        self.hdlgen_path = hdlgen_path
+        # The very first thing we want to do is check
+        #   0) Read the HDLGen XML for relevant filepaths
+        #   1) Does the folder we need exist
+        #   2) Does the XML we need exist
+        #   3) Validate the XML
+        # Done
+        pass
+
+    def something(self):
+        pass
