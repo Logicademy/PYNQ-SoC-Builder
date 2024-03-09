fdb15d6b21c2da5e9b259335c86b0dc461a3a898
lukecanny <lcanny8@gmail.com>
2024-03-09 00:00:04 +0000

    added custom exception

--- application/checks..py
+++ application/checks..py
@@ -1,5 +1,10 @@
 import os
 
+class DashesInHDLFileError(Exception):
+    def __init__(self, message="Error: --- Dashes found in HDL Source File, have you completed HDLGen-ChatGPT workflow?"):
+        self.message = message
+        super().__init__(self.message)
+
 def check_for_dashes(path_to_hdl):
     try:
         with open(path_to_hdl, 'r') as file:
@@ -14,8 +19,10 @@ def check_for_dashes(path_to_hdl):
 
                 if architecture_found and line.startswith("---"):
                     print("Found line starting with '---' after 'architecture':", line)
+                    raise DashesInHDLFileError
                     # You can add further processing or break out of the loop here if needed
 
+        print(f"{path_to_hdl} passed --- check.")
     except FileNotFoundError:
         print(f"Error: File '{path_to_hdl}' not found.")
         raise FileNotFoundError
