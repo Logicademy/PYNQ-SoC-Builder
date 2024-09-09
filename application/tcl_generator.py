29e7183b65d09a037a9ea0f5f86b0a0a04a9e5a2
lukecanny <lcanny8@gmail.com>
2024-09-09 19:20:58 +0100

    fixed an import

--- application/tcl_generator.py
+++ application/tcl_generator.py
@@ -2,7 +2,7 @@ import xml.dom.minidom
 import os
 import re
 import application.xml_manager as xmlman
-from config import *
+from application.config import *
 # tcl_generator.py
 # This Python 3 script is responsible for generating a Tcl script file dynamically depending on the project.
 
